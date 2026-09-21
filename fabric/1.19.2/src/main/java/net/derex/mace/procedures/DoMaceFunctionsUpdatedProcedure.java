886289d148826ecc85f36c3a4d409df6047ac74e
Derek <derex1987@gmail.com>
2026-09-21 00:54:29 -0500

    deleting old forge classes

--- fabric/1.19.2/src/main/java/net/derex/mace/procedures/DoMaceFunctionsUpdatedProcedure.java
+++ fabric/1.19.2/src/main/java/net/derex/mace/procedures/DoMaceFunctionsUpdatedProcedure.java
@@ -2,17 +2,16 @@ package net.derex.mace.procedures;
 
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.advancements.AdvancementProgress;
 import net.minecraft.advancements.Advancement;
@@ -35,14 +34,14 @@ public class DoMaceFunctionsUpdatedProcedure {
 		double remainingDamage = 0;
 		double rawDamage = 0;
 		fallHeight = Math.ceil(sourceentity.fallDistance);
-		remainingFallHeight = fallHeight;/*code*/
+		remainingFallHeight = fallHeight;
 		playerBaseDamage = ((LivingEntity) sourceentity).getAttribute(net.minecraft.world.entity.ai.attributes.Attributes.ATTACK_DAMAGE).getValue();
 		firstDamage = Math.min(remainingFallHeight, 3) * 4;
 		remainingFallHeight = remainingFallHeight - 3;
 		secondDamage = Math.min(remainingFallHeight, 5) * 4;
 		remainingFallHeight = remainingFallHeight - 5;
 		rawDamage = firstDamage + secondDamage + remainingDamage;
-		densityDmg = fallHeight * 0.5 * itemstack.getEnchantmentLevel(MaceBackportModEnchantments.DENSITY);
+		densityDmg = fallHeight * 0.5 * EnchantmentHelper.getItemEnchantmentLevel(MaceBackportModEnchantments.DENSITY, itemstack);
 		fallDmg = 1.5 * (rawDamage + densityDmg);
 		sourceentity.fallDistance = 0;
 		sourceentity.setDeltaMovement(new Vec3(0, 0, 0));
@@ -54,23 +53,23 @@ public class DoMaceFunctionsUpdatedProcedure {
 					fallDmg = fallDmg;
 				}
 			}
-			if (itemstack.getEnchantmentLevel(MaceBackportModEnchantments.BREACH) > 0) {
+			if (EnchantmentHelper.getItemEnchantmentLevel(MaceBackportModEnchantments.BREACH, itemstack) > 0) {
 				{
 					Entity _entToDamage = entity;
-					_entToDamage.hurt(new DamageSource(_entToDamage.level.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.GENERIC)), (float) (fallDmg * 1.15));
+					_entToDamage.hurt(DamageSource.GENERIC, (float) (fallDmg * 1.15));
 				}
 			} else {
 				{
 					Entity _entToDamage = entity;
-					_entToDamage.hurt(new DamageSource(_entToDamage.level.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.GENERIC)), (float) fallDmg);
+					_entToDamage.hurt(DamageSource.GENERIC, (float) fallDmg);
 				}
 			}
 			if (entity instanceof EnderDragon) {
 				{
 					Entity _entToDamage = entity;
-					_entToDamage.hurt(new DamageSource(_entToDamage.level.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.GENERIC)), (float) fallDmg);
+					_entToDamage.hurt(DamageSource.GENERIC, (float) fallDmg);
 				}
-			} /*Nonessential Functions below after essential functions execute*/
+			}
 			if (world instanceof ServerLevel _level)
 				_level.sendParticles(ParticleTypes.EXPLOSION, x, (y + 1.5), z, 25, 4, 0, 4, 1);
 			if (!entity.isOnGround()) {
--- fabric/1.19.2/src/main/java/net/derex/mace/procedures/WindBurstFunctionProcedure.java
+++ fabric/1.19.2/src/main/java/net/derex/mace/procedures/WindBurstFunctionProcedure.java
@@ -1,10 +1,10 @@
 package net.derex.mace.procedures;
 
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.damagesource.DamageSource;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
@@ -14,18 +14,15 @@ import net.derex.mace.init.MaceBackportModEnchantments;
 public class WindBurstFunctionProcedure {
 
 	public static void execute(Entity entity, Entity sourceentity) {
-		execute(null, entity, sourceentity);
-	}
-
-	private static void execute(Entity entity, Entity sourceentity) {
 		if (entity == null || sourceentity == null)
 			return;
 		double windBounceLevel = 0;
 		double fallHeight = 0;
-		if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getEnchantmentLevel(MaceBackportModEnchantments.WIND_BURST) > 0) {
+		ItemStack itemstack = sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY;
+		if (EnchantmentHelper.getItemEnchantmentLevel(MaceBackportModEnchantments.WIND_BURST, itemstack) > 0) {
 			fallHeight = sourceentity.fallDistance;
 			if (fallHeight > 1.5) {
-				windBounceLevel = (sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getEnchantmentLevel(MaceBackportModEnchantments.WIND_BURST);
+				windBounceLevel = EnchantmentHelper.getItemEnchantmentLevel(MaceBackportModEnchantments.WIND_BURST, itemstack);
 				sourceentity.setDeltaMovement(new Vec3((sourceentity.getDeltaMovement().x()), (1 + windBounceLevel * 0.1), (sourceentity.getDeltaMovement().z())));
 				entity.fallDistance = 0;
 			}
