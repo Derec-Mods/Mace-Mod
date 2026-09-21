19f351db94812175655d2a34be54d481048f5e67
Derek <derex1987@gmail.com>
2026-09-21 01:36:01 -0500

    remove creative tag

--- fabric/1.19.2/src/main/java/net/derex/mace/item/MaceItem.java
+++ fabric/1.19.2/src/main/java/net/derex/mace/item/MaceItem.java
@@ -6,8 +6,8 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.entity.LivingEntity;
-import net.derex.mace.init.MaceBackportModTabs;
 import net.derex.mace.procedures.DoMaceFunctionsUpdatedProcedure;
 
 public class MaceItem extends SwordItem {
@@ -36,7 +36,7 @@ public class MaceItem extends SwordItem {
 			public Ingredient getRepairIngredient() {
 				return Ingredient.of(new ItemStack(Blocks.IRON_BLOCK));
 			}
-		}, 3, -2.4f, new Item.Properties().tab(MaceBackportModTabs.MACE_TAB));
+		}, 3, -2.4f, new Item.Properties().tab(CreativeModeTab.TAB_COMBAT));
 	}
 
 	@Override
--- fabric/1.20.1/src/main/java/net/derex/mace/init/MaceBackportModTabs.java
+++ fabric/1.20.1/src/main/java/net/derex/mace/init/MaceBackportModTabs.java
@@ -1,27 +1,12 @@
 package net.derex.mace.init;
 
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
-import net.derex.mace.MaceBackportMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.world.item.CreativeModeTabs;
 
 public class MaceBackportModTabs {
-	public static final ResourceKey<CreativeModeTab> MACE_TAB_KEY = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(MaceBackportMod.MODID, "mace_tab"));
-	public static final CreativeModeTab MACE_TAB = FabricItemGroup.builder()
-		.icon(() -> new ItemStack(MaceBackportModItems.MACE))
-		.title(Component.translatable("itemGroup.mace_backport.mace_tab"))
-		.displayItems((itemDisplayParameters, output) -> {
-			output.accept(MaceBackportModItems.MACE);
-		})
-		.build();
-
 	public static void registerCreativeTabs() {
-		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, MACE_TAB_KEY, MACE_TAB);
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT).register(entries -> {
+			entries.accept(MaceBackportModItems.MACE);
+		});
 	}
 }
